81b72c0c6eacd6b9c6bd02f54e28fe2084965060
meharaz733 <meharaz733@gmail.com>
2025-10-25 19:58:20 +0600

    [Improve] code structure

--- backend/Model/modelAndTalk.py
+++ backend/Model/modelAndTalk.py
@@ -3,16 +3,58 @@ from langchain_huggingface import (
     ChatHuggingFace,
     HuggingFaceEndpoint
 )
+from langchain_chroma import Chroma
+from Model.embed_fn import embedding
+import os
+from Utils.prompt import promptForChat
+from Utils.makeItReadyForChat import isVideoExist
+from langchain.chains import create_retrieval_chain
+from langchain.chains.combine_documents import create_stuff_documents_chain
 
 
-model_ = HuggingFaceEndpoint(
+llm_model = HuggingFaceEndpoint(
     repo_id = "meta-llama/Llama-3.1-8B-Instruct",
     task = "text_generation"
 )
 
-_model = ChatHuggingFace(llm = model_)
+model = ChatHuggingFace(llm = llm_model)
 
 
-def talkWithChatModel(prompt):
-    model_response = _model.invoke(prompt)
-    return model_response.content
+def talkWithChatModel(userQuery, sessionID, userChatHistory, videoID, dbPath):
+
+    session_id = sessionID
+    
+    #existing session ID for existing collection..
+    existSessionID = isVideoExist(videoID=videoID, dbPath=dbPath)
+    if existSessionID:
+        session_id = existSessionID
+
+    try:
+        vectorStore = Chroma(
+            collection_name=session_id,
+            embedding_function=embedding,
+            chroma_cloud_api_key=os.getenv("CHROMA_API_KEY"),
+            tenant=os.getenv("CHROMA_TENANT"),
+            database=os.getenv("CHROMA_DATABASE")
+        )
+    except Exception as e:
+        return f"ERROR: Database connenction error\n{e}"
+    
+    chatPrompt = promptForChat(userChatHistory)
+    
+    questionAndAnswerChain = create_stuff_documents_chain(
+        llm=model,
+        prompt=chatPrompt
+    )
+    chain = create_retrieval_chain(
+        vectorStore.as_retriever(),
+        questionAndAnswerChain
+    )
+
+    try:
+        model_response = chain.invoke({'input': userQuery})["answer"]
+    except Exception as e:
+        print(f"ERROR: {e}")
+        model_response = "Sorry, I couldn't generate a respones at the moment."
+
+    return model_response
--- backend/main.py
+++ backend/main.py
@@ -6,23 +6,15 @@ from Schema.data_model import (   #pydantic model to validate client and api rep
     startdata
 )
 from Utils.makeItReadyForChat import (
-    MakeItReadyForChat,
-    isVideoExist
+    MakeItReadyForChat
 )
-from Model.modelAndTalk import _model
 from Utils.chatHistory import ChatHistory
-# from retrieve import RetrieveContentFromVectorStore
-from Utils.prompt import promptForChat
 import uuid
 from Database.database import Database
-from langchain.chains import create_retrieval_chain
-from langchain.chains.combine_documents import create_stuff_documents_chain
 from fastapi.middleware.cors import CORSMiddleware
-from langchain_chroma import Chroma
 from dotenv import load_dotenv
-import os
-from Model.embed_fn import embedding
 import langchain
+from Model.modelAndTalk import talkWithChatModel
 
 langchain.debug = True
 load_dotenv()
@@ -56,12 +48,13 @@ async def getSessionID(data: startdata):
     """
     Generate a secure random session ID and perform preprocces for chat.
     """
-
-    print(data.video_url)
     sessionID = str(uuid.uuid4())
     video_id = MakeItReadyForChat(data.video_url, sessionID, db.get_db_path())
     
-    return SessionIDData(sessionID = sessionID, videoID = video_id)
+    return SessionIDData(
+        sessionID = sessionID,
+        videoID = video_id
+    )
 
 
 
@@ -72,49 +65,16 @@ async def chat(req_body: UserData):
     """
 
     chat = ChatHistory(req_body.sessionID, dbPath=db.get_db_path())
-
-    #load previous chat...
     userChatHistory = chat.__loadMessage__(50) #last 50 messages
 
-    session_id = req_body.sessionID
-
-    #old session ID for existing collection...
-    existSessionID = isVideoExist(req_body.videoID, dbPath=db.get_db_path())
-    if existSessionID:
-        session_id = existSessionID
-
-    try:
-        vectorStore = Chroma(
-            collection_name=session_id,
-            embedding_function=embedding,
-            chroma_cloud_api_key=os.getenv("CHROMA_API_KEY"),
-            tenant=os.getenv("CHROMA_TENANT"),
-            database=os.getenv("CHROMA_DATABASE")
-        )
-    except Exception as e:
-        return ResponseData(
-            sessionID = req_body.sessionID,
-            aiAnswer = "ERROR: Connenction error",
-            videoID = req_body.videoID
-        )
-    chatPrompt = promptForChat(userChatHistory)
+    model_response = talkWithChatModel(
+                        req_body.user_query,
+                        req_body.sessionID,
+                        userChatHistory,
+                        req_body.videoID,
+                        db.get_db_path()
+                    )
     
-    questionAndAnswerChain = create_stuff_documents_chain(
-        llm=_model,
-        prompt=chatPrompt
-    )
-    chain = create_retrieval_chain(
-        vectorStore.as_retriever(),
-        questionAndAnswerChain
-    )
-
-    #send to llm(chatmodel)...
-    try:
-        model_response = chain.invoke({'input': req_body.user_query})["answer"]
-    except Exception as e:
-        print(f"ERROR: {e}")
-        model_response = "Sorry, I couldn't generate a respones at the moment."
-
     # store the chat...
     chat.__saveMessage__([("human", req_body.user_query),("ai", model_response)])
     
